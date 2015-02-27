373b35e3a3d1124925c369fc654f4361c0f5a189
Dun Liu <dliu@vistaprint.com>
2015-02-27 15:53:31 -0500

    Initial commit

---
+++ src/main/java/dun/maven/App.java
@@ -0,0 +1,13 @@
+package dun.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
